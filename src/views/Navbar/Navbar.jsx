9f00a9e28b3cc9022ee79c4d09c7159dbef6e542
mariorodrigueeez <mariorodiguezjob@gmail.com>
2023-08-27 20:03:02 +0200

    Añadido Boton de descarga CV, falta version movil

--- src/views/Navbar/Navbar.jsx
+++ src/views/Navbar/Navbar.jsx
@@ -7,6 +7,7 @@ import LogoCV3 from '../../assets/LogoCV3.png'
 const navigation = [
   { name: 'Home', href: 'home', current: true },
   { name: 'Experience', href: '#', current: false },
+  { name: 'Skils', href: '#', current: false },
   { name: 'Projects', href: '#', current: false },
   { name: 'Contact', href: '#', current: false },
 ]
@@ -14,11 +15,11 @@ const navigation = [
 
 export default function Navbar() {
   return (
-    <Disclosure as="nav" className="bg-gray-800">
+    <Disclosure as="nav" className="c_marron_pastel">
       {({ open }) => (
         <>
           <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
-            <div className="relative flex h-16 items-center justify-between">
+            <div className="relative flex h-16 items-center justify-between ">
               <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
                 {/* Mobile menu button*/}
                 <Disclosure.Button className="inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
@@ -49,20 +50,24 @@ export default function Navbar() {
 
               </div>
 
-              <div className="hidden sm:m-auto sm:block ">
+              <div className="hidden sm:m-auto sm:block">
                 <div className="flex space-x-4 ">
                   {navigation.map((item) => (
                     <a
                       key={item.name}
                       href={item.href}
-                      className="block rounded-md px-3 py-2 text-base font-medium text-white hover:bg-gray-700"
+                      className="block rounded-md px-3 py-2 text-base font-medium text-white c3_hover"
                     >
                       {item.name}
                     </a>
                   ))}
                 </div>
               </div>
+              <div className=" ">
+                <button type='button' className='botonS1 rounded-lg px-3 py-2 text-base font-medium'>Dowload CV</button>
 
+
+              </div>
             </div>
           </div>
 
